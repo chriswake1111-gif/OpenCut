bdc26bb6607a0bf0983d4d2194201d6df91bb5e3
chriswake1111-gif <chriswake1111@gmail.com>
2026-06-22 10:45:09 +0800

    1

---
+++ run_opencut.bat
@@ -0,0 +1,6 @@
+@echo off
+title OpenCut Launcher
+echo =======================================
+echo     Starting OpenCut Desktop App...
+echo =======================================
+cargo run -p opencut-desktop
